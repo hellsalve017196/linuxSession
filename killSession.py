9f80ad23b1cb2c3e130c68c63a64549171bdac3e
hellsalve017196 <abdullah017196@gmail.com>
2020-06-21 00:42:42 +0100

    updated

--- killSession.py
+++ killSession.py
@@ -5,15 +5,16 @@ def killSession(screenId):
     subprocess.check_output(cmd, shell=True)
 
 def loopThroughSession():
-    try:
-        f = open('output.txt', 'r')
-        for x in f:
-            if x != '':
-                screen_id = int(x.split('.')[0])
-                killSession(screen_id)
-        f.close()
-    except Exception as e:
-        pass
+    f = open('output.txt', 'r')
+    
+    for x in f:
+        try:
+            screen_id = int(x.split('.')[0])
+            killSession(screen_id)
+        except Exception as e:
+            pass
+    
+    f.close()
 
 
 
--- killSession.sh
+++ killSession.sh
@@ -1,1 +1,1 @@
-screen -list | grep -v PI_BOY,direct > output.txt && python killSession.py
+screen -list | grep -v PI_BOY,direct > output.txt && python killSession.py && rm -rf output.txt
