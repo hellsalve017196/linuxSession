29dcd31874152de9c797729db38b6a0eedb860fa
hellsalve017196 <abdullah017196@gmail.com>
2020-06-21 00:54:48 +0100

    update

--- killSession.sh
+++ killSession.sh
@@ -1,1 +1,1 @@
-screen -list | grep -v PI_BOY,direct > output.txt && python killSession.py && rm -rf output.txt
+screen -list | grep -v PI_BOY | grep -v direct > output.txt && python killSession.py && rm -rf output.txt
